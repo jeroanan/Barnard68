e04213883f823ac891b828b8f2844c13c5a172aa
David Wilson <davidwil@posteo.de>
2021-02-17 21:22:02 +0000

    Squashed commit of the following:
    commit e1a18e1ea6458f2e91c2f76c522ca59193319188
    Author: David Wilson <davidwil@posteo.de>
    Date:   Wed Feb 17 21:21:48 2021 +0000
    
        wikimedia reader language
    
    commit bdf94832df4ef12b454982ea0604b2fe3dd5f5c3
    Author: David Wilson <davidwil@posteo.de>
    Date:   Wed Feb 17 21:03:37 2021 +0000
    
        wip

--- src/wiktionary.rkt
+++ src/wiktionary.rkt
@@ -1,5 +1,4 @@
 #!/usr/bin/env racket
-#lang s-exp "wikimedia.rkt"
+#lang reader "wikimedia.rkt"
 
-(wikimedia-set-base-url "en.wiktionary.org")
-(wikimedia-begin)
+wikimedia-base-url en.wiktionary.org
